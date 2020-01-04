920ad1aa8a9d74b20bfa3a7b11d8cc633ce2220e
liu288 <935622585@qq.com>
2020-01-04 19:17:35 +0800

    MD5加密测试

--- Utils/src/test/java/encode/EncodeTest.java
+++ Utils/src/test/java/encode/EncodeTest.java
@@ -1,19 +1,20 @@
+package encode;
+
 import com.utils.encode.MD5Utils;
 import org.junit.Test;
 
 public class EncodeTest {
 
+    /**
+     *  MD5加密测试
+     */
     @Test
     public void test() {
         String s = "abc";
 
-        String res = MD5Utils.MD5Encode(s, null);
+        String res = MD5Utils.MD5Encode(s);
 
         System.out.println(res);
-
-        String res2 = MD5Utils.MD5Encode(s, null);
-
-        System.out.println(res2);
     }
 
 }
