219fd539450b4cf28ff7a885450e09e5edbce0a6
liu288 <935622585@qq.com>
2020-01-04 19:09:04 +0800

    MD5加密测试修改

--- Utils/src/main/java/com/utils/encode/MD5Utils.java
+++ Utils/src/main/java/com/utils/encode/MD5Utils.java
@@ -2,20 +2,35 @@ package com.utils.encode;
 
 import java.security.MessageDigest;
 
+/**
+ * MD5加密工具类
+ */
 public class MD5Utils {
 
-    private static final String hexDigIts[] = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};
+    private static final String hexDigIts[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
     /**
      * MD5加密
-     * @param origin 字符
-     * @param charsetname 编码
-     * @return
+     * MD5属于单向加密
+     *
+     * @param origin 字符串
+     * @return 加密后的字符串
      */
-    public static String MD5Encode(String origin, String charsetname){
+    public static String MD5Encode(String origin) {
+        return MD5Encode(origin, "UTF-8");
+    }
+
+    /**
+     * MD5加密
+     *
+     * @param origin      字符串
+     * @param charsetname 字符串的编码  如："UTF-8"
+     * @return 加密后的字符串
+     */
+    public static String MD5Encode(String origin, String charsetname) {
         String resultString = null;
         try {
-            resultString = new String(origin);
+            resultString = origin;
             MessageDigest md = MessageDigest.getInstance("MD5");
             if (null == charsetname || "".equals(charsetname)) {
                 resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
@@ -28,18 +43,17 @@ public class MD5Utils {
         return resultString;
     }
 
-
-    public static String byteArrayToHexString(byte b[]){
+    private static String byteArrayToHexString(byte b[]) {
         StringBuffer resultSb = new StringBuffer();
-        for(int i = 0; i < b.length; i++){
+        for (int i = 0; i < b.length; i++) {
             resultSb.append(byteToHexString(b[i]));
         }
         return resultSb.toString();
     }
 
-    public static String byteToHexString(byte b){
+    private static String byteToHexString(byte b) {
         int n = b;
-        if(n < 0){
+        if (n < 0) {
             n += 256;
         }
         int d1 = n / 16;
--- Utils/src/test/java/EncodeTest.java
+++ Utils/src/test/java/EncodeTest.java
@@ -5,11 +5,15 @@ public class EncodeTest {
 
     @Test
     public void test() {
-        String s = "abd";
+        String s = "abc";
 
         String res = MD5Utils.MD5Encode(s, null);
 
         System.out.println(res);
+
+        String res2 = MD5Utils.MD5Encode(s, null);
+
+        System.out.println(res2);
     }
 
 }
